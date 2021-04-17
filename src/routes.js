efde267f94c688e6c1ec84eee003ce6528effc06
Jilson Martins Junior <jilsonmartins.jr@gmail.com>
2021-04-17 20:32:11 -0300

    Commit inicial

--- src/routes.js
+++ src/routes.js
@@ -12,12 +12,12 @@ const routes = express.Router()
 const views = __dirname + "/views/" // foi necessario pq ejs entende que views esta na raiz, e não na pasta src(source)
 
 const profile = {
-    name: "Jilson",
+    name: "Jilson Martins Júnior",
     avatar: "https://avatars.githubusercontent.com/u/66040353?v=4",
     "monthly-budget": 3000,
     "days-per-week": 5,
-    "hours-per-day": 5,
-    "vacation-per-year": 4,
+    "hours-per-day": 8,
+    "vacation-per-year": 4
 }
 
 routes.get('/', (req, res) => res.render(views + "index"))
